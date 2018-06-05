e0735df2e2c627cb43e2d95e08288eea29f31902
wisniad <wisniad@gmail.com>
2018-06-05 09:45:07 +0200

    Add bootstrap

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
-Vue.config.productionTip = false
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
